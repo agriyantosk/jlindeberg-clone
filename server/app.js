f2f41cc26e3d67c1292b7298fafbbc519fe7c999
agriyantosk <agriyantosk@gmail.com>
2024-10-09 00:32:43 +0700

    fix: port env

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 // require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const cors = require("cors");
 const router = require("./routers");
 const { errorHandler } = require("./middlewares/errorhandler");
@@ -15,5 +15,5 @@ app.use(errorHandler);
 //require router dengan path nya
 //panggil cors dengan require disini dan panggil dengan cara use invoke //gunakan ulrencoded invoke object extended dengan value true //gunakan express json yang di invokasi agar bisa menangkap data login //gunakan router
 app.listen(port, () => {
-    console.log(`We are in the planet no... ${port}`);
+  console.log(`We are in the planet no... ${port}`);
 });
